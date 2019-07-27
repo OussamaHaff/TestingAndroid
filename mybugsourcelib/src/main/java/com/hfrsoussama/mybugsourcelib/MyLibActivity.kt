99a3abc6ad225bf62b2ae6aa4b32cffdb4acbc21
Oussama HAFFERSSAS <hfrs.oussama@gmail.com>
2019-07-27 13:24:03 +0200

    Reproduced the app bundle resource error

---
+++ mybugsourcelib/src/main/java/com/hfrsoussama/mybugsourcelib/MyLibActivity.kt
@@ -0,0 +1,12 @@
+package com.hfrsoussama.mybugsourcelib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyLibActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_lib)
+    }
+}
